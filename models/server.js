95615a171a1f34ab7b357b9e6630bf4d2ecd4cc1
Marcelo Maturana <marcelo.maturana@absolutecode.cl>
2021-10-18 14:37:51 -0300

    ajuste ruta api/user -> api/users

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
-    this.usersPath = "/api/user";
+    this.usersPath = "/api/users";
 
     //Middlewares
     this.middlewares();
